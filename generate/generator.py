3ddac650db3a82a4986aceac12ac1e1020f03599
thlurte <thlurte@gmail.com>
2024-06-16 05:10:54 +0530

    feat: generate

---
+++ generate/generator.py
@@ -0,0 +1,37 @@
+import os
+import google.generativeai as genai
+
+class load_llm:
+    def __init__(self,top_k,results,prompt) -> None:
+        genai.configure(api_key='AIzaSyBb8mq-RCYt0wTMtu6oqHc8GBoNLphJjTU')
+        self.generation_config = {
+            "temperature": 1,
+            "top_p": 0.95,
+            "top_k": 64,
+            "max_output_tokens": 8192,
+            "response_mime_type": "text/plain",
+        }
+        self.llm = genai.GenerativeModel(
+            model_name="gemini-1.5-flash",
+            generation_config=self.generation_config,
+        )
+        self.chat_session = self.llm.start_chat(
+        history=[]
+        )
+        self.top_k = top_k
+        self.results = results
+        self.context = self._context_formatting()
+        self.prompt = prompt
+
+    def _context_formatting(self):
+        context=""
+        for i in list(self.top_k)[1]:
+            context = context+self.results[i.numpy()]['sentence_chunk']
+        return context
+
+    def chat(self):
+        self.response = self.chat_session.send_message("Using the following context, answer the question below"+self.context+"\n\n"+self.prompt)
+        return self.response.text
+
+
+        
--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 from preprocess.indexing import chunking, process_pdf 
 from embed.embedding import load_model
 from retrieve.retriever import similarity_search
+from generate.generator import load_llm
 document = process_pdf('/home/ahmed/Downloads/b.pdf')
+
 text_chunks = chunking(10,document.open_and_read_pdf()).chunk_and_split_text()
+
 embedding_model = load_model(text_chunks)
-retriever = similarity_search(query="What is concurrency",text=embedding_model.encode())
+embeddings = embedding_model.encode()
+retriever = similarity_search(query="What is concurrency",text=embeddings)
+
 top_k = retriever.retrieve()
 
-
+llm = load_llm(top_k, embeddings,"What is concurrency")
+print(llm.chat())
